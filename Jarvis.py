fc93810784c6ca7b2c16391d64bfb27339203208
Arpan Soni <75475025+arpan2002@users.noreply.github.com>
2021-06-22 15:35:44 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -50,18 +50,18 @@ def takeCommand():
          return "None"
     return query
 
-# def sendEmail(to, content):
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.ehlo()
-#     server.starttls()
-#     server.login('youremail@gmail.com', 'your-password')
-#     server.sendmail('youremail@gmail.com', to, content)
-#     server.close()
+ def sendEmail(to, content):
+     server = smtplib.SMTP('smtp.gmail.com', 587)
+     server.ehlo()
+     server.starttls()
+     server.login('youremail@gmail.com', 'your-password')
+     server.sendmail('youremail@gmail.com', to, content)
+     server.close()
 
 if __name__ == "__main__":
     wishMe()
     while True:
-        if 1:
+        if:
          query = takeCommand().lower()
 
           # Logic for executing tasks based on query
@@ -73,38 +73,38 @@ if __name__ == "__main__":
            print(results)
            speak(results)
 
-        #    elif 'open youtube' in query:
-        #     webbrowser.open("youtube.com")
+             elif 'open youtube' in query:
+             webbrowser.open("youtube.com")
 
-        #    elif 'open google' in query:
-        #     webbrowser.open("google.com")
+            elif 'open google' in query:
+             webbrowser.open("google.com")
 
-        #    elif 'open stackoverflow' in query:
-        #     webbrowser.open("stackoverflow.com")   
+            elif 'open stackoverflow' in query:
+             webbrowser.open("stackoverflow.com")   
 
 
-        #    elif 'play music' in query:
-        #     music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-        #     songs = os.listdir(music_dir)
-        #     print(songs)    
-        #     os.startfile(os.path.join(music_dir, songs[0]))
+            elif 'play music' in query:
+             music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+             songs = os.listdir(music_dir)
+             print(songs)    
+             os.startfile(os.path.join(music_dir, songs[0]))
 
-        #    elif 'the time' in query:
-        #     strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-        #     speak(f"Sir, the time is {strTime}")
+            elif 'the time' in query:
+             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+             speak(f"Sir, the time is {strTime}")
 
-        #    elif 'open code' in query:
-        #     codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-        #     os.startfile(codePath)
+            elif 'open code' in query:
+             codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+             os.startfile(codePath)
 
-        #    elif 'email to harry' in query:
-        #     try:
-        #         speak("What should I say?")
-        #         content = takeCommand()
-        #         to = "harryyourEmail@gmail.com"    
-        #         sendEmail(to, content)
-        #         speak("Email has been sent!")
-        #     except Exception as e:
-        #         print(e)
-        #         speak("Sorry my friend Arpan Soni. I am not able to send this email")    
+            elif 'email to harry' in query:
+             try:
+                 speak("What should I say?")
+                 content = takeCommand()
+                 to = "harryyourEmail@gmail.com"    
+                 sendEmail(to, content)
+                 speak("Email has been sent!")
+             except Exception as e:
+                 print(e)
+                 speak("Sorry my friend Arpan Soni. I am not able to send this email")    
 
